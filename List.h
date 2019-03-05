ca7b449d0e94997338009b46c294554eec38df34
Austin Nadler <austinnadler706@gmail.com>
2019-03-05 15:22:00 -0600

    testing command line git

--- List.h
+++ List.h
@@ -15,9 +15,13 @@ class List {
 private:
     using elementPtr_t = E *;
 
+    // This the the dynamic array.
     elementPtr_t elements = nullptr;
     int numberOfElements = 0;
 
+
+    /*----------------------------------------------------------------*/
+    // Utility methods
     void copyElements(const List& copy) {
         numberOfElements = copy.numberOfElements;
         elements = new E[numberOfElements];
@@ -32,19 +36,27 @@ private:
         elements = nullptr;
         numberOfElements = 0;
     }// end
+    /*----------------------------------------------------------------*/
 
 public:
-    List() { }// end List()
+    List() { /* field declarations initialize the object. */ }// end List()
 
+    // The big three.
     List(const List& copy) { copyElements(copy); }// end List()
     virtual ~List()        { releaseElements();  }// end ~List()
+    
     const List& operator =(const List& rhs) {
+        /* Check for self-assignment. */
         if (this != &rhs) {
             releaseElements();
             copyElements(rhs);
         }
+
+        // Return a reference to the object itself.
         return *this;
     }// end =()
+    /*----------------------------------------------------------------*/
+
     void pushBack(const E& element)  { pushAt(numberOfElements, element); }// end pushBack()
     void pushFront(const E& element) { pushAt(0, element);                }// end pushFront()
     void pushAt(const int index, const E& element) {
@@ -58,12 +70,14 @@ public:
         } else {
             elementPtr_t tmpElements = new E[numberOfElements + 1];
 
+            // Copy all elements to the tmp array, starting from the back.
             for (int i = numberOfElements - 1; i >= index; i--) {
                 tmpElements[i + 1] = elements[i];
             }
-
+            // Add the new value to the tmp array.
             tmpElements[index] = element;
 
+            // Continue copying the elements array.
             for (int i = index - 1; i >= 0; i--) {
                 tmpElements[i] = elements[i];
             }
@@ -74,6 +88,7 @@ public:
         }
         numberOfElements += 1;
     }// end
+    /*----------------------------------------------------------------*/
 
     E popBack()  { return popAt(numberOfElements - 1); }// end popBack()
     E popFront() { return popAt(0);                    }// end popFront()
@@ -84,19 +99,21 @@ public:
 
         elementPtr_t tmpElements = new E[numberOfElements - 1];
 
+        // Copy all elements to the tmp array, starting from the back.
         for (int i = numberOfElements - 1; i > index; i--) {
             tmpElements[i - 1] = elements[i];
         }
-
+        // Remove the value to the tmp array.
         E valuePopped = tmpElements[index];
 
+        // Continue copying the elements array.
         for (int i = index - 1; i >= 0; i--) {
             tmpElements[i] = elements[i];
         }
 
         delete [] elements;
         elements = tmpElements;
-        tmpElements = nullptr; // not delete because that would delete the memory pointed to by tmpElements
+        tmpElements = nullptr;
 
         numberOfElements -= 1;
         return valuePopped;
@@ -122,6 +139,7 @@ public:
 
         numberOfElements -= 1;
     }// end
+    /*----------------------------------------------------------------*/
 
     E getBack() const  { return getAt(numberOfElements - 1); }// end getBack()
     E getFront() const { return getAt(0);                    }// end getFront()
@@ -131,17 +149,41 @@ public:
         }
         return elements[index];
     }// end
-    
+    /*----------------------------------------------------------------*/
+
     int size() const     { return numberOfElements;      }// end size()
     bool isEmpty() const { return numberOfElements == 0; }// end isEmpty()
+    /*----------------------------------------------------------------*/
 
+    virtual string toString() const {
+        ostringstream oss;
+
+        oss << "size: " << numberOfElements << ", [ ]";
+        if ( !isEmpty() ) {
+            // Remove ']' from the stream
+            oss.seekp(-1, ios::end);
+
+            for (int i = 0; i <= numberOfElements - 1; i++) {
+                oss << elements[i] << ", ";
+            }
+
+            // Remove the ', ' from the stream.
+            oss.seekp(-2, ios::end);
+            oss << " ]";
+        }
+
+        return oss.str();
+    }// end toString()
+    /*----------------------------------------------------------------*/
 };// end List
 
 
+/*----------------------------------------------------------------*/
 template <typename E>
 ostream& operator <<(ostream& os, const List<E>& o) {
     os << o.toString();
     return os;
 }// end <<()
+/*----------------------------------------------------------------*/
 
 #endif
